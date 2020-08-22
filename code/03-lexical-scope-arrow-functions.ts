e0d1d795554a1e1e172989371c1ed8e2d82a7a69
Gassan <g_fox_fts@yahoo.com>
2020-08-23 00:25:20 +0200

    03-lexical-scope-arrow-functions

---
+++ code/03-lexical-scope-arrow-functions.ts
@@ -0,0 +1,13 @@
+class MyClass {
+    myMethod() {
+      const foo = 123;
+      console.log('1', this);
+      setTimeout(() => {
+        console.log('2', this);
+      }, 0);
+    }
+  }
+  
+  const myInstance = new MyClass();
+  myInstance.myMethod();
+  
