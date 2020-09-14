fb0d96100474e7332ee3389ccbe644939754781b
Gassan <g_fox_fts@yahoo.com>
2020-09-14 16:15:55 +0200

    enum numeric

---
+++ code/23-enum-numeric.ts
@@ -0,0 +1,13 @@
+enum Sizes {
+    Small,
+    Medium,
+    Large,
+  }
+enum Sizes {
+    ExtraLarge = 3,
+  }
+  
+  const selectedSize = 2;
+  
+  console.log(Sizes.Large, Sizes[selectedSize]);
+  
